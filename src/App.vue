fb755f05bb6d22f1c15877454eb9d6a34b57b244
jotajotaramos <jramosj@gmail.com>
2020-10-07 13:47:13 +0200

    Creating the assistants list component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <AssistantsTitle/>
+    <AssistantsList/>
   </div>
 </template>
 
 <script>
 import AssistantsTitle from './components/AssistantsTitle.vue'
+import AssistantsList from './components/AssistantsList.vue'
 
 export default {
   name: 'App',
   components: {
-    AssistantsTitle
+    AssistantsTitle,
+    AssistantsList
   }
 }
 </script>
---
+++ src/components/AssistantsList.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div>
+        <ul v-for="person in peopleList" :key="person">
+            <li>{{person}}</li>
+        </ul>
+        <div>
+            <button @click="addAssistant()">Añadir Asistente</button>
+            <button @click="deleteAssistant()">Eliminar Asistente</button>
+        </div>    
+    </div>
+    
+</template>
+
+<script>
+export default {
+
+    data: function (){
+        return{
+            peopleList:['Ana', 'Juan', 'Pepe', 'Victoria', 'Antonio'],
+        }
+    },
+    methods: {
+        addAssistant: function(){
+            var randomNumber = Math.floor(Math.random() * 10001);
+            this.peopleList.push(`Asistente${randomNumber}`);
+        },
+        deleteAssistant: function(){
+            this.peopleList.splice(0,1);
+        }
+    }
+
+}
+</script>
+
